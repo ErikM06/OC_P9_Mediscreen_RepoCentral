646ef125e0173e963e49cb080a2e63c2ddc7f9e9
ErikM06 <murer.erik@gmail.com>
2022-05-25 17:22:41 +0200

    first build commit

---
+++ src/main/java/com/mediscreen/repoCentral/RepoCentralApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.repoCentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoCentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediscreen/repoCentral/RepoCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.repoCentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
